2aaaa89434ccb87c290422e3cf7ca40d5f4c84c1
porl <porl42@gmail.com>
2020-05-18 15:34:25 +1000

    fixed multiplayer crash

--- src/main/java/info/u_team/useful_railroads/block/SpeedClampRailBlock.java
+++ src/main/java/info/u_team/useful_railroads/block/SpeedClampRailBlock.java
@@ -9,34 +9,35 @@ import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.IBlockReader;
 
 public class SpeedClampRailBlock extends CustomPoweredRailBlock {
-	
+
 	public SpeedClampRailBlock(String name) {
 		super(name);
 	}
-	
+
 	@Override
 	protected void controllSpeed(BlockPos pos, BlockState state, AbstractMinecartEntity cart) {
 		final double clampVelocity = 0.25D; // hard-coded for now
 		final double currentVelocity = cart.getMotion().length();
 
-		// if speed too slow (may cause issue with cartDirection) use playerDirection
-		Vec3d direction;
-        if (currentVelocity < 0.1D) {
-        	direction = Minecraft.getInstance().player.getMotion().normalize();
+		Vec3d direction = Vec3d.ZERO;
+		if (currentVelocity < 0.1D) {
+			if (cart.getPassengers().size() > 0) {
+				direction = cart.getPassengers().get(0).getMotion().normalize();
+			}
 		} else {
-        	direction = cart.getMotion().normalize();
+			direction = cart.getMotion().normalize();
 		}
 
-		cart.setVelocity(
-				direction.getX() * clampVelocity,
-				direction.getY() * clampVelocity,
-				direction.getZ() * clampVelocity
-				);
+		cart.setMotion(new Vec3d(
+			direction.getX() * clampVelocity,
+			direction.getY() * clampVelocity,
+			direction.getZ() * clampVelocity
+		));
 	}
-	
+
 	@Override
 	public boolean canMakeSlopes(BlockState state, IBlockReader world, BlockPos pos) {
 		return false;
 	}
-	
+
 }
